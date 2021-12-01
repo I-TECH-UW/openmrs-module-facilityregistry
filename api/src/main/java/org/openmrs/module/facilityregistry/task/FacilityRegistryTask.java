dbef58fa98b64c167c2a04a862bd64b2f58a614f
mozzy11 <mozzymutesa@gmail.com>
2021-12-01 14:45:13 +0300

    add licecence Headers to all files

--- api/src/main/java/org/openmrs/module/facilityregistry/task/FacilityRegistryTask.java
+++ api/src/main/java/org/openmrs/module/facilityregistry/task/FacilityRegistryTask.java
@@ -1,3 +1,12 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 package org.openmrs.module.facilityregistry.task;
 
 import java.io.IOException;
@@ -105,7 +114,7 @@ public class FacilityRegistryTask extends AbstractTask {
 	/**
 	 * Saves/Updates Fhir Location into the OpenMRS database
 	 * 
-	 * @param searchBundle Bundle fetched form the Facility Registry Server
+	 * @param searchBundle Bundle fetched fromm the Facility Registry Server
 	 */
 	private void saveFhirLocation(Bundle searchBundle) {
 		for (BundleEntryComponent entry : searchBundle.getEntry()) {
