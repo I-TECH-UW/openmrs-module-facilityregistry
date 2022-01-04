e5f84a71dfaba33b3a7ab1e0176d231b9ca04520
mozzy11 <mozzymutesa@gmail.com>
2022-01-04 23:19:16 +0300

    add tests

--- integration-tests/src/test/java/org/openmrs/module/facilityregistry/providers/r4/OrganizationFhirResourceProviderIntegrationTest.java
+++ integration-tests/src/test/java/org/openmrs/module/facilityregistry/providers/r4/OrganizationFhirResourceProviderIntegrationTest.java
@@ -9,6 +9,21 @@
  */
 package org.openmrs.module.facilityregistry.providers.r4;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.startsWith;
+
 import org.openmrs.module.facilityregistry.providers.r4.OrganizationFhirResourceProvider;
 import org.openmrs.module.fhir2.providers.r4.BaseFhirR4IntegrationTest;
 
@@ -20,10 +35,13 @@ import org.hl7.fhir.r4.model.Organization;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletResponse;
 
 public class OrganizationFhirResourceProviderIntegrationTest extends BaseFhirR4IntegrationTest<OrganizationFhirResourceProvider, Organization> {
 	
 	private static final String ORGANIZATION_INITIAL_DATA_XML = "test_data/FhirOrganizationTestdata.xml";
+	
+	private static final String ORGANIZATION_UUID = "cf9b1f44-0e8f-42f1-900b-bf1c5d4ed5CC";
 	
 	@Autowired
 	@Getter(AccessLevel.PUBLIC)
@@ -33,13 +51,14 @@ public class OrganizationFhirResourceProviderIntegrationTest extends BaseFhirR4I
 	@Override
 	public void setup() throws Exception {
 		super.setup();
-		
 		executeDataSet(ORGANIZATION_INITIAL_DATA_XML);
 	}
 	
 	@Test
-	public void shouldReturnExistingObservationAsJson() throws Exception {
-		System.out.println(".................>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<");
+	public void shouldReturnExistingOrganizationAsJson() throws Exception {
+		MockHttpServletResponse response = get("/Organization/" + ORGANIZATION_UUID).accept(FhirMediaTypes.JSON).go();
+		
+		//assertThat(response, isOk());
 	}
 	
 }
